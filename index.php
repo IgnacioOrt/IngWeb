d65cf1359ffc09ada49c7dbec7d53268c6a5029a
IGNACIOORT <comosea1122@hotmail.com>
2018-11-11 23:19:51 -0600

    navbar

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Uel Uelik</title>
+	<link rel="stylesheet" href="dist/css/bootstrap.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body style="background-color: rgb(100,100,0);">
+	<!-- Top menu -->
+	<nav class="navbar navbar-dark fixed-top navbar-expand-md navbar-no-bg">
+    	<div class="container">
+	        <a class="navbar-brand" href="index.php">UEL UELIK</a>
+        	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+	            <span class="navbar-toggler-icon"></span>
+        	</button>
+        	<div class="collapse navbar-collapse" id="navbarNav">
+	            <ul class="navbar-nav">
+                	<li class="nav-item">
+	                    <a class="nav-link scroll-link" href="consulta.php">Consulta de receta</a>
+                	</li>
+                	<li class="nav-item">
+	                    <a class="nav-link scroll-link" href="noticias.php">Noticias</a>
+                	</li>
+	                <li class="nav-item">
+                    	<a class="nav-link scroll-link" href="acerca.php">Acerca de</a>
+                	</li>
+                	
+            	</ul>
+            	<span class="ml-auto navbar-nav">
+            		<li class="nav-item dropdown">
+            			<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            				Iniciar sesión
+            			</a>
+            			<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            				<a class="dropdown-item" href="#">Action</a>
+            				<a class="dropdown-item" href="#">Another action</a>
+            				<a class="dropdown-item" href="#">Something else here</a>
+            			</div>
+            		</li>
+            	</span>
+        	</div>
+    	</div>
+	</nav>
+	<script src="dist/jquery/jquery.slim.min.js"></script>
+	<script src="dist/js/bootstrap.min.js"></script>
+	<script src="dist/popper/umd/popper.min.js"></script>
+</body>
+</html>
